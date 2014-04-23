c6389ab7a6f53146379e7b160484ffcdc6d55070
knoopd <knoopd@me.com>
2014-04-23 19:21:10 -0400

    inital commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('#headerBlock').load('../includes/headerNavBar.html');
+});
+$(document).ready(function () {
+    $('#footerBlock').load('../includes/footer.html');
+});
